1cac0a2a358b4bb796bc64f0f74ceda900f0bfb6
Evgenia Fedotova <fedotovaevgenia1793@gmail.com>
2023-07-07 21:39:46 +0400

    "коммит"

--- app.js
+++ app.js
@@ -2,8 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const ERROR_CODE = 400;
-
 const NOT_FOUND_ERROR_CODE = 404;
 
 const { PORT = 3000 } = process.env;
@@ -27,7 +25,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.use('*', (req, res) => res.status(NOT_FOUND_ERROR_CODE).send({ message: ERROR_CODE }));
+app.use('*', (req, res) => {
+  res.status(NOT_FOUND_ERROR_CODE).send({ message: 'Запрашиваемый ресурс не найден' });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
